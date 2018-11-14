e3b7b88109cbe8a480753e9e1b4846f19e5112dd
Adnan <Adnan@QA-003.mshome.net>
2018-11-14 16:55:21 +0500

    Assignment Number 1

--- First Assignment/src/Assignmentone/Firstclass.java
+++ First Assignment/src/Assignmentone/Firstclass.java
@@ -13,35 +13,97 @@ public class Firstclass {
 		System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("http://172.30.106.102:8085/Unison/jsp/login/login.jsf");
+		driver.get("https://demo.opencart.com/index.php?route=account/register");
 		driver.manage().window().maximize();
 		
 		
 		String actualpagetitle = driver.getTitle();
-		String expectedtitle = "Login Page";
+		String expectedtitle = "Register Account";
+		
 		
 		if (actualpagetitle.contentEquals(expectedtitle))
 		{
-			WebElement username;
-			username = driver.findElement(By.id("loginForm:LoginId"));
-			username.sendKeys("admin");
+			
+			WebElement FirstName;
+			FirstName = driver.findElement(By.id("input-firstname"));
+			FirstName.sendKeys("Muhammad Adnan");
+			
+			WebElement Lastname;
+			Lastname = driver.findElement(By.id("input-lastname"));
+			Lastname.sendKeys("Adil");
+			
+			WebElement email;
+			email = driver.findElement(By.id("input-email"));
+			email.sendKeys("adnan.adil@yahoo.com");
+			
+			WebElement Phone;
+			Phone = driver.findElement(By.id("input-telephone"));
+			Phone.sendKeys("03337042374");
 			
 			WebElement Password;
-			Password = driver.findElement(By.id("loginForm:pass"));
-			Password.sendKeys("");
+			Password = driver.findElement(By.id("input-password"));
+			Password.sendKeys("1234567890");
 			
-			WebElement loginButton;
-			loginButton = driver.findElement(By.xpath("//*[@id=\"loginForm:loginSubmitButton\"]"));
-			loginButton.click();
+			WebElement ConPassword;
+			ConPassword = driver.findElement(By.id("input-confirm"));
+			ConPassword.sendKeys("1234567890");
+			
+			WebElement Checkbox;
+			Checkbox = driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[1]"));
+			Checkbox.click();
+			
+			
+			WebElement ContinueButton;
+			ContinueButton = driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div/input[2]"));
+			ContinueButton.click();
+			
+			WebElement Continuebtn;
+			Continuebtn = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a"));
+			Continuebtn.click();
+			
+			WebElement dropdown;
+			dropdown = driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a"));
+			dropdown.click();
+			
+			WebElement logoutbtn;
+			logoutbtn = driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[5]/a"));
+			logoutbtn.click();
+			
+			WebElement conbtnafterlogout;
+			conbtnafterlogout = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a"));
+			conbtnafterlogout.click();
+			
+			
+			driver.get("https://demo.opencart.com/index.php?route=account/login");
+			
+			WebElement Login;
+			Login = driver.findElement(By.id("input-email"));
+			Login.sendKeys("adnan.adil@yahoo.com");		
+			
+			WebElement loginPassword;
+			loginPassword = driver.findElement(By.id("input-password"));
+			loginPassword.sendKeys("1234567890");
+			
+			WebElement Logintbtn;
+			Logintbtn = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+			Logintbtn.click();
+			
+			driver.close();
+			
+			
 		}
 		else
 		{
 			System.out.println("Test Failed");
 		}
 		
+
+
+	}
+
+	private static void close() {
+		// TODO Auto-generated method stub
 		
-
-
 	}
 
 }
